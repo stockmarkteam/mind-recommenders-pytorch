35dc76c219c27b4888897599c1250541136c01ab
Ali Cevahir <ali.cevahir@stockmark.co.jp>
2022-05-10 16:15:08 +0900

    perceiver_io recommender branch

---
+++ libs/perceiver-io/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='perceiver-io-pytorch',
+    version='0.1.4',
+    packages=['perceiver_io'],
+    package_dir={'': 'src'},
+    url='https://github.com/esceptico/perceiver-io',
+    license='MIT',
+    author='Timur Ganiev',
+    author_email='ganiev.tmr@gmail.com',
+    description='Unofficial Perceiver IO implementation',
+    install_requires=[
+        'torch==1.9.0',
+        'einops==0.3.0'
+    ]
+)
